cfc63f27263d3047471b0bc0bfa1d8e19d23e4be
yohans14 <yohans14@yahoo.com>
2022-03-14 10:56:06 -0500

    created product route with CRUD methode

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +9,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () =>
+		console.log(`Now listening http://localhost:${PORT} `)
+	);
 });
